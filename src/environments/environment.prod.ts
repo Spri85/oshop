d13b5b7749ffeb935e7ff76a64fbe17b531a80fb
Viktor <viktorpar@gmail.com>
2017-12-16 09:47:22 +0200

    added bootstrap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDmF3GC4RZxCRQbpAJVw-Y6Hvzy-zjNugs',
+    authDomain: 'oshop-d7cf5.firebaseapp.com',
+    databaseURL: 'https://oshop-d7cf5.firebaseio.com',
+    projectId: 'oshop-d7cf5',
+    storageBucket: '',
+    messagingSenderId: '687091574860'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDmF3GC4RZxCRQbpAJVw-Y6Hvzy-zjNugs',
+    authDomain: 'oshop-d7cf5.firebaseapp.com',
+    databaseURL: 'https://oshop-d7cf5.firebaseio.com',
+    projectId: 'oshop-d7cf5',
+    storageBucket: '',
+    messagingSenderId: '687091574860'
+  }
 };
